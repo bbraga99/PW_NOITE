adcadef22af1f31610fb40c41a8b321938e01917
bbraga99 <brunuu.henrique@hotmail.com>
2022-08-19 21:45:02 -0300

    Aula 19-08-2022

--- index.php
+++ index.php
@@ -1,8 +1,51 @@
-<?php   
+<h1>
+    <?php   
 
-// comentario em php - linha
-/*
-    comentario em php - em um bloco de codigo
-*/
+        /*$nota1 = 2;
+        $nota2 = 8;
+        $media = ($nota1+$nota2)/2;
 
-?>
+        if ($media >= 6) {
+            echo "Aprovado";
+        } else {
+            echo "Reprovado";
+        }
+        */
+
+        //concatenação
+        $nome = 'Bruno';
+        echo $nome." "."BRAGA";
+
+        //Loop em PHP 
+        // DO WHILE
+        $contadora = 0;
+        do {
+                //código a ser repetido
+                $contadora ++;
+        } while ($contadora < 10);
+
+        //WHILE
+        $cont = 0;
+        while($cont < 10) {
+            //executa os codigos
+            $cont ++;
+        }
+
+        //FOR 
+
+        for ($i = 0; $i < 10; $i++) {
+            //executa o código
+
+            if($i % 2 == 0) {
+                echo "é par <br>";
+            } else {
+                echo "é impar <br>";
+            }
+        }
+
+        
+
+
+
+    ?>
+</h1>
